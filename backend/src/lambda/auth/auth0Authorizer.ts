34aa007ce8e477e07d6e80f1c721d2a68f2b7e64
Taiwo Ogunola <taiwo.ogunola.dev@gmail.com>
2021-11-07 14:12:24 +0100

    feat: verifyToken func

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -9,10 +9,7 @@ import { JwtPayload } from '../../auth/JwtPayload'
 
 const logger = createLogger('auth')
 
-// TODO: Provide a URL that can be used to download a certificate that can be used
-// to verify JWT token signature.
-// To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = '...'
+const jwksUrl = 'https://dev-8ad8olgg.us.auth0.com/.well-known/jwks.json'
 
 export const handler = async (
   event: CustomAuthorizerEvent
@@ -56,12 +53,32 @@ export const handler = async (
 
 async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
-  const jwt: Jwt = decode(token, { complete: true }) as Jwt
+  const { data: jwks } = await Axios.get(jwksUrl)
 
-  // TODO: Implement token verification
-  // You should implement it similarly to how it was implemented for the exercise for the lesson 5
-  // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-  return undefined
+  const keys: any[] = jwks.keys
+  const jwt: Jwt = decode(token, { complete: true }) as Jwt
+  const signingKeys = keys
+    .filter(
+      (key) =>
+        key.use === 'sig' &&
+        key.kty === 'RSA' &&
+        key.kid &&
+        key.x5c &&
+        key.x5c.length
+    )
+    .map((key) => {
+      return { kid: key.kid, nbf: key.nbf, x5c: key.x5c[0] }
+    })
+  const signingKey = signingKeys.find((key) => key.kid === jwt.header.kid)
+  let certValue: string = signingKey.x5c
+
+  certValue = certValue.match(/.{1,64}/g).join('\n')
+  const finalCertKey: string = `-----BEGIN CERTIFICATE-----\n${certValue}\n-----END CERTIFICATE-----\n`
+  let jwtPayload: JwtPayload = verify(token, finalCertKey, {
+    algorithms: ['RS256']
+  }) as JwtPayload
+
+  return jwtPayload
 }
 
 function getToken(authHeader: string): string {
