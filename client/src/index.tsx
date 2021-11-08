cc8dd94a1517d7b12bbdb16ac7f87bb8a840bdb1
Taiwo Ogunola <taiwo.ogunola.dev@gmail.com>
2021-11-08 12:16:02 +0100

    feat: fix client bugs with expected API format

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,9 +1,8 @@
-import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import * as serviceWorker from './serviceWorker'
 import 'semantic-ui-css/semantic.min.css'
-import { makeAuthRouting } from './routing';
+import { makeAuthRouting } from './routing'
 
 ReactDOM.render(makeAuthRouting(), document.getElementById('root'))
 
