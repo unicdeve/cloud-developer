64c1353beeae5ff4d899262b1e8af7950b07e695
Taiwo Ogunola <taiwo.ogunola.dev@gmail.com>
2021-11-08 10:49:52 +0100

    feat: fix deployment issues

--- backend/src/dataLayer/todosAccess.ts
+++ backend/src/dataLayer/todosAccess.ts
@@ -1,8 +1,5 @@
 import * as AWS from 'aws-sdk'
-import * as AWSXRay from 'aws-xray-sdk'
 import { DocumentClient } from 'aws-sdk/clients/dynamodb'
-
-const XAWS = AWSXRay.captureAWS(AWS)
 
 const logger = createLogger('auth')
 
@@ -135,11 +132,11 @@ export class TodosAccess {
 function createDynamoDBClient() {
   if (process.env.IS_OFFLINE) {
     console.log('Creating a local DynamoDB instance')
-    return new XAWS.DynamoDB.DocumentClient({
+    return new AWS.DynamoDB.DocumentClient({
       region: 'localhost',
       endpoint: 'http://localhost:8000'
     })
   }
 
-  return new XAWS.DynamoDB.DocumentClient()
+  return new AWS.DynamoDB.DocumentClient()
 }
